f89f992eb49de5038b4c46edcf2ca73e9c6ceced
strike277 <d3nizzeMaldonado@gmail.com>
2026-04-07 15:46:04 -0600

    Hotfix/traslados (#112)
    * fix: role level
    
    * fix: all rates url

--- src/presentation/routes/api/servicesRoutes.js
+++ src/presentation/routes/api/servicesRoutes.js
@@ -119,7 +119,7 @@ router.get(
 router.get(
   '/:id/all-rate-prices',
   jwtMiddleware.authenticateToken,
-  jwtMiddleware.requireRoleLevel(6), // Admin and above
+  jwtMiddleware.requireRoleLevel(4), // Department Manager and above
   (req, res) => ServicesController.getAllRatePricesForService(req, res)
 );
 
@@ -132,7 +132,7 @@ router.get(
 router.get(
   '/:id/all-rate-prices-with-client-prices',
   jwtMiddleware.authenticateToken,
-  jwtMiddleware.requireRoleLevel(6), // Admin and above
+  jwtMiddleware.requireRoleLevel(4), // Department Manager and above
   (req, res) => ServicesController.getAllRatePricesForServiceWithClientPrices(req, res)
 );
 
@@ -186,7 +186,7 @@ router.get(
 router.get(
   '/:id',
   jwtMiddleware.authenticateToken,
-  jwtMiddleware.requireRoleLevel(6), // Admin and above
+  jwtMiddleware.requireRoleLevel(4), // Department Manager and above
   (req, res) => ServicesController.getServiceById(req, res)
 );
 
